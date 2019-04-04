71fd041dc960367e6449e920d5f7bcc2c92eca8c
nikni <nicola.tommasi.91@gmail.com>
2019-04-04 20:14:34 +0200

    Adding style to the demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { NgxFeedbackService } from 'ngx-feedback';
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
     constructor(private readonly feedbackService: NgxFeedbackService) {}
